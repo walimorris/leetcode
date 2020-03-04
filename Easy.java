c702e0ffc848f1f6e213c6300a872c5e0a2844cc
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-03-04 00:08:31 -0800

    Update Easy.java

--- Easy.java
+++ Easy.java
@@ -384,3 +384,50 @@ public class ToTheRight {
         return a;
     }
 }
+
+/* Given a sorted(in ascending order) integer array nums of n elements 
+ * and a target value, write a function to search target in nums. If 
+ * target exists, then return its index, otherwise return -1
+ *
+ * Author : Wali Morris 
+ * File   : BinarySearch.java
+ * Date   : 03/04/2020
+ */
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        // test 1
+        int[] input1 = {-1, 0, 3, 5, 9, 12};
+        int key1 = 9;
+        int output1 = search(input1, key1);
+        System.out.println("Key is at index: " + output1);
+
+        // test 2
+        int[] input2 = {-1, 0, 3, 5, 9, 12};
+        int key2 = 2;
+        int output2 = search(input2, key2);
+        System.out.println("Key is at index: " + output2);
+
+        // test 3
+        int[] input3 = {1, 4, 7, 9, 11, 13, 15, 16, 21, 55, 66, 68, 99, 123, 156, 202};
+        int key3 = 21;
+        int output3 = search(input3, key3);
+        System.out.println("Key is at index: " + output3);
+    }
+
+    public static int search(int[] nums, int key) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high) {
+            int mid = (high + low) / 2;
+            if( nums[mid] == key) {
+                return mid;
+            } else if(key < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
