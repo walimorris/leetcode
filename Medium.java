66491b4ee71a7daec39427728039f67f59af2469
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-04-03 00:39:30 -0700

    Update Medium.java

--- Medium.java
+++ Medium.java
@@ -12,7 +12,7 @@
  * into a 32-bit integer without overflowing. 
  * 
  * ProducsOfNumbers object will be instantiated and called as such : 
- * ProductOfNumbers obj = new ProductOfNumbers(k); 
+ * ProductOfNumbers obj = new ProductOfNumbers(); 
  * obj.add(num); 
  * int param_2 = obj.getProduct(k); 
  *
