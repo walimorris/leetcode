b1779a8e4673761a8461b7096a99d3ac9a193b27
Wali Morris <48896622+walimorris@users.noreply.github.com>
2020-09-18 02:43:01 -0700

    Update Easy.java

--- Easy.java
+++ Easy.java
@@ -5404,3 +5404,30 @@ public class ReverseVowels2 {
         return s2.toString();
     }
 }
+
+/**
+ * Best Time to Buy and Sell Stocks
+ * 
+ * Say you have an array for which the ith element is the price of a given stock on day i. If you were only permitted to complete
+ * at most one transaction ( buy one and sell on share of stock), design an algorithm to find the maximum profit. Note: you cannot 
+ * sell a stock before you buy one. 
+ * 
+ * @author Wali Morris 
+ * @since 09/18/2020
+ */
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;                                         // initialize profit to zero dollars
+        for ( int i = 0; i < prices.length; i++ ) {                // outer loop begins
+            int buy = prices[i];                                   // buying price
+            for ( int j = i; j < prices.length; j++ ) {            // inner loop begins 
+                int sell = prices[j];                              // selling price 
+                if ( j > i && sell > buy ) {                       // selling price is after buying price and selling price is higher
+                   maxProfit = Math.max(maxProfit, sell - buy);    // take the highest profit so far
+                } 
+            } 
+        } 
+        return maxProfit;                                          // return max profit 
+    } 
+} 
