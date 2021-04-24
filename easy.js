0a5a5f260e5665290984947aede2d45f4fb550c7
Wali Morris <48896622+walimorris@users.noreply.github.com>
2021-04-24 13:16:59 -0700

    Update easy.js

--- easy.js
+++ easy.js
@@ -132,3 +132,29 @@ const truncateSentence = function(s, k) {
     }
     return sentence;
 }
+
+/**
+ * You are given two strings word1 and word2. Merge the strings by 
+ * adding letters in alternating order, starting with word1. If a 
+ * string is longer than the other, append the additional letters 
+ * onto the end of the merged string. Return the merged string.
+ * 
+ * @author WaliMorris<walimmorris@gmail.com>
+ * @param word1
+ * @param word2
+ * @returns {string}
+ * 
+ */
+let mergeAlternately = function(word1, word2) {
+    const longest = word1.length > word2.length ? word1.length : word2.length;
+    let mergedString = '';
+    for (let index = 0; index < longest; index++) {
+        if (index < word1.length) {
+            mergedString += word1[index];
+        }
+        if (index < word2.length) {
+            mergedString += word2[index];
+        }
+    }
+    return mergedString;
+}
