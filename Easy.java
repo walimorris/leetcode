8555202f54ad531a27abe2cfa4112dbfef765ffb
Wali Morris <48896622+walimorris@users.noreply.github.com>
2021-06-11 13:26:57 -0700

    Update Easy.java

--- Easy.java
+++ Easy.java
@@ -6665,3 +6665,74 @@ public class Main {
         return containingWords.get(0);
     }
 }
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Write an algorithm to determine if a number n is happy.
+ *
+ * A happy number is a number defined by the following process:
+ *
+ *   1. Starting with any positive integer, replace the number
+ *   by the sum of the squares of its digits.
+ *
+ *   2. Repeat the process until the number equals 1(where it
+ *   will stay), or it loops endlessly in a cycle which does
+ *   not include 1.
+ *
+ *   3. Those numbers for which this process ends in 1 are happy.
+ *
+ *   Return true if n is a happy number, and false if not.
+ *
+ * @author Wali Morris<walimmorris@gmail.com>
+ */
+public class Main {
+    public static void main(String[] args) {
+        int input1 = 19;
+        int input2 = 2;
+
+        boolean output1 = isHappy(input1);
+        boolean output2 = isHappy(input2);
+
+        System.out.println(output1);
+        System.out.println(output2);
+    }
+
+    public static boolean isHappy(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        Set<Integer> seen = new HashSet<>();
+        int size = 0;
+
+        while (n != 1) {
+            // n is in range 1 - 9
+            if (n < 10) {
+                n = n * n;
+            }
+            // n is more than one digit
+            int digit;
+            if (n > 9) {
+                int sum = 0;
+                // get n as string
+                String nStr = Integer.toString(n);
+
+                // square each digit and find the sum of all digits of n
+                for (int i = 0; i < nStr.length(); i++) {
+                    digit = Integer.parseInt(String.valueOf(nStr.charAt(i)));
+                    sum += (digit * digit);
+                }
+                n = sum;
+                seen.add(n);
+
+                // this sum has been seen twice
+                if (seen.size() == size) {
+                    return false;
+                }
+                size++;
+            }
+        }
+        return true;
+    }
+}
