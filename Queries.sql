605f86202b48559579d03e050c394c4036a97a06
Wali Morris <48896622+walimorris@users.noreply.github.com>
2023-04-03 12:56:22 -0700

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -30,3 +30,6 @@
   */
   
  SELECT tweet_id FROM Tweets WHERE LENGTH(content) > 15; 
+
+# Write an SQL query to report all the duplicate emails. Note that it's # guaranteed that the email field is not NULL.
+SELECT email AS 'Email' FROM Person GROUP BY email HAVING COUNT(email) > 1;
