e2bf09b820b615a49a9a32dbba9b3820eb38f99a
Wali Morris <48896622+walimorris@users.noreply.github.com>
2023-04-04 13:11:07 -0700

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -36,3 +36,9 @@
   */ 
   
  SELECT email AS 'Email' FROM Person GROUP BY email HAVING COUNT(email) > 1;
+ 
+ /*
+  * Write a SQL query to report the first name, last name, city and state of each person in the Person table. If the address
+  * of a personId is not present in the Address table, report null instead.
+  */
+  SELECT p.firstName, p.lastName, a.city, a.state FROM Person AS p LEFT JOIN Address AS a ON p.personId = a.personId;
