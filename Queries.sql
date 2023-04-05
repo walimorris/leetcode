9486a1a438eb3893c7ef932a7e028d98283b71ee
Wali Morris <48896622+walimorris@users.noreply.github.com>
2023-04-05 00:30:03 -0700

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -77,3 +77,15 @@
    WHERE 
        b.bonus < 1000 || b.bonus IS NULL;
      
+ /*
+  * Write an SQL query to find all the authors that viewed at least one of their own articles.
+  * Return the result table sorted by id in ascending order.
+  */
+   SELECT DISTINCT
+       author_id AS 'id'
+   FROM 
+       Views
+   WHERE 
+       author_id = viewer_id
+   ORDER BY 
+       author_id ASC;
