437ab133cb02f8dca1cd49aa1c4fa8e79cba7db5
Wali Morris <48896622+walimorris@users.noreply.github.com>
2023-04-05 23:46:25 -0700

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -89,3 +89,13 @@
        author_id = viewer_id
    ORDER BY 
        author_id ASC;
+       
+/*
+ * Write an SQL query to report the names of the customer that are not referred by the customer with id = 2.
+ */
+  SELECT 
+      name
+  FROM 
+      Customer
+  WHERE 
+      referee_id <> 2 OR referee_id IS NULL;
