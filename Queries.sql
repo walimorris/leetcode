aa06d61e51bc44eae60b71f8d466afa5412d0867
Wali Morris <48896622+walimorris@users.noreply.github.com>
2023-04-06 03:04:06 -0700

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -111,3 +111,18 @@
       class 
   HAVING 
       COUNT(class) >= 5;
+      
+/*
+ * Write a SQL query for a report that provides the pairs (actor_id, director_id) where the actor has cooperated 
+ * with the director at least three times.
+ */
+  SELECT 
+      actor_id, 
+      director_id
+  FROM 
+      ActorDirector
+  GROUP BY
+      actor_id, 
+      director_id
+  HAVING 
+      COUNT(timestamp) >= 3;
